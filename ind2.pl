82fb0d4536b2341f92bb49ced3429c7e1648b101
ChernayaJuly <62398248+ChernayaJuly@users.noreply.github.com>
2020-04-19 22:56:53 +0300

    Add files via upload

---
+++ ind2.pl
@@ -0,0 +1,39 @@
+read_list(A,N):-r(A,[],0,N).
+r(A,A,N,N):-!.
+r(A,B,X,N):-X1 is X+1,read(D),append(B,[D],B1),r(A,B1,X1,N).
+
+delete_all([],B,B,_):-!.
+delete_all([H|T],B,C,X):-H=X,delete_all(T,B,C,X).
+delete_all([H|T],B,C,X):-append(C,[H],C1),delete_all(T,B,C1,X).
+
+find([H|_],H).
+find([_|T],E):-find(T,E).
+
+count([],_,K,K):-!.
+count([H|T],H,K,K1):-K1 is K+1,count(T,H,K,K1),!.
+count([_|T],EL,K,K1):-count(T,EL,K,K1).
+
+new_list([],B,B):-!.
+new_list([H|T],C,B):-find(T,H),count([H|T],H,K,0),(K>3 -> append(C,[H],C1),new_list(T,C1,B);delete_all([H|T],A1,[],H),new_list(A1,C,B)).
+new_list([_|T],C,B):-append(C,T,C1),new_list(C1,C,B).
+
+ind2:-read(N),read_list(A,N),new_list(A,[],B),write(B).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
