cd00e719819c242efa16f1a522659a65cc71fa6b
Julia <c.julia@rambler.ru>
2020-05-03 20:29:57 +0300

    ind4 конечное

--- ind 4.pl
+++ ind 4.pl
@@ -4,7 +4,7 @@ list([_|T],E):-list(T,E).
 
 ind4:-People=[_,_,_],
                      list(People,[aladar,_,_]),
-                     list(People,[bel,_,_]),
+                     list(People,[bela,_,_]),
                      list(People,[balash,_,_]),
                      list(People,[_,aptekar,_]),
                      list(People,[_,buchgalter,_]),
@@ -13,8 +13,26 @@ ind4:-People=[_,_,_],
                      list(People,[_,_,bekeshchab]),
                      list(People,[_,_,asod]),
 
-                     not(list(People,[balash,aptekar,_])),
-                     not(list(People,[balash,_,budapesht])),
-                     not(list(People,[balash,_,asod])),
-                     not(list(People,[aladar,buchgalter,_])),
+                    not(list(People,[balash,aptekar,_])),
+                    not(list(People,[balash,_,budapesht])),
+                    list(People,[_,aptekar,budapesht]),
+                    ((list(People,[aladar,agronom,asod]),
+                      list(People,[bela,buchgalter,budapesht]));
+                     (list(People,[aladar,agronom,asod]),
+                     list(People,[bela,buchgalter,bekeshchab]));
+                     (list(People,[aladar,agronom,asod]),
+                     list(People,[balash,buchgalter,budapesht]));
+                     (list(People,[aladar,agronom,asod]),
+                     list(People,[balash,buchgalter,bekeshchab]));
+                     (list(People,[aladar,aptekar,asod]),
+                     list(People,[balash,buchgalter,bekeshchab]));
+                     (list(People,[aladar,aptekar,asod]),
+                     list(People,[balash,buchgalter,budapesht]));
+                     (list(People,[aladar,aptekar,asod]),
+                     list(People,[bela,buchgalter,bekeshchab]));
+                     (list(People,[aladar,aptekar,asod]),
+                     list(People,[bela,buchgalter,budapesht]))),
+ write(People),nl,!.
+
+
 
