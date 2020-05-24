a6795485bcefd9ee7d6e9475952bb739c3af280d
Julia <c.julia@rambler.ru>
2020-05-24 22:35:30 +0300

    ind11 конечное

--- инд11/инд11/Program.fs
+++ инд11/инд11/Program.fs
@@ -8,18 +8,19 @@ open System.Drawing
 let form = new Form(Text = "Площадь прямоугольника")
 let button = new Button(Text = "вычислить площадь",Left=80, Top = 200,Height=30,Width = 100) 
 form.Controls.Add(button)
-button.Click.Add(fun evArgs -> MessageBox.Show("Площадь прямоугольника = ") |> ignore)
 
-let a_label = new Label(Text = "введите длину", Left=20, Top = 30,Height=30,Width = 100)
+let a_label = new Label(Text = "введите длину", Left=20, Top = 20,Height=30,Width = 100)
 form.Controls.Add(a_label)
-let a_text = new TextBox(Left=25, Top =30,Height=30,Width = 150)
+let a_text = new TextBox(Left=120, Top =20,Height=30,Width = 80)
 form.Controls.Add(a_text)
 
 let b_label = new Label(Text = "введите ширину",Left=20, Top = 80,Height=30,Width = 100)
 form.Controls.Add(b_label)
-let b_text = new TextBox(Left=25, Top = 80,Height=30,Width = 150)
+let b_text = new TextBox(Left=120, Top = 80,Height=40,Width = 80)
 form.Controls.Add(b_text)
-ооваомаимолла
+
+button.Click.Add(fun evArgs -> MessageBox.Show("Площадь прямоугольника = "+(int(a_text.Text)*int(b_text.Text)).ToString()) |> ignore)
+
 Application.Run(form)
 
 [<EntryPoint>]
